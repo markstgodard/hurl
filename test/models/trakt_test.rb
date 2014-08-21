cf0b49cfd44d6683946410bf765b23422f94559c
Mark St.Godard <markstgodard@gmail.com>
2014-08-21 09:51:21 -0500

    Update trakt test, changing data, should move over to VCR to avoid brittle tests

--- test/models/trakt_test.rb
+++ test/models/trakt_test.rb
@@ -9,11 +9,11 @@ class TraktTest < ActiveSupport::TestCase
 
   test "search by episode" do
     json = Trakt::fetch_media_info(@api_key, "Breaking+Bad+S05E12+", :shows)
-    assert_equal "http://slurm.trakt.us/images/fanart/126.55.jpg",
+    assert_equal "http://slurm.trakt.us/images/fanart/126.56.jpg",
                   json[:art]
 
     # this will be the screen shot not the regular poster
-    assert_equal "http://slurm.trakt.us/images/episodes/126-5-12.55.jpg",
+    assert_equal "http://slurm.trakt.us/images/episodes/126-5-12.56.jpg",
                   json[:poster]
 
   end
@@ -22,7 +22,7 @@ class TraktTest < ActiveSupport::TestCase
     json = Trakt::fetch_media_info(@api_key, "Breaking+Bad+", :shows)
 
     # this will be a generic poster
-    assert_equal "http://slurm.trakt.us/images/posters/126.55.jpg",
+    assert_equal "http://slurm.trakt.us/images/posters/126.56.jpg",
                   json[:poster]
   end
 
